ff57bf3339be5b149ed3b7af206508f86d58048d
HilthonTT <timothy.tandt@outlook.be>
2026-08-09 17:38:13 +0200

    added make_disk_image.sh

--- src/bootloader/stage2/main.c
+++ src/bootloader/stage2/main.c
@@ -42,55 +42,55 @@ void __attribute__((cdecl)) start(uint16_t bootDrive)
   }
   FAT_Close(fd);
 
-  const int desiredWidth = 1024;
-  const int desiredHeight = 768;
-  const int desiredBpp = 32;
-  uint16_t pickedMode = 0xffff;
+  // const int desiredWidth = 1024;
+  // const int desiredHeight = 768;
+  // const int desiredBpp = 32;
+  // uint16_t pickedMode = 0xffff;
 
-  // Initialize graphics
-  VbeInfoBlock *info = (VbeInfoBlock *)MEMORY_VESA_INFO;
-  VbeModeInfo *modeInfo = (VbeModeInfo *)MEMORY_MODE_INFO;
-  if (VBE_GetControllerInfo(info))
-  {
-    uint16_t *mode = (uint16_t *)(info->VideoModePtr);
-    for (int i = 0; mode[i] != 0xFFFF; i++)
-    {
-      if (!VBE_GetModeInfo(mode[i], modeInfo))
-      {
-        printf("Can't get mode info %x :(\n", mode[i]);
-        continue;
-      }
-      bool hasFB = (modeInfo->attributes & 0x90) == 0x90;
+  // // Initialize graphics
+  // VbeInfoBlock *info = (VbeInfoBlock *)MEMORY_VESA_INFO;
+  // VbeModeInfo *modeInfo = (VbeModeInfo *)MEMORY_MODE_INFO;
+  // if (VBE_GetControllerInfo(info))
+  // {
+  //   uint16_t *mode = (uint16_t *)(info->VideoModePtr);
+  //   for (int i = 0; mode[i] != 0xFFFF; i++)
+  //   {
+  //     if (!VBE_GetModeInfo(mode[i], modeInfo))
+  //     {
+  //       printf("Can't get mode info %x :(\n", mode[i]);
+  //       continue;
+  //     }
+  //     bool hasFB = (modeInfo->attributes & 0x90) == 0x90;
 
-      if (hasFB && modeInfo->width == desiredWidth && modeInfo->height == desiredHeight && modeInfo->bpp == desiredBpp)
-      {
-        pickedMode = mode[i];
-        break;
-      }
-    }
+  //     if (hasFB && modeInfo->width == desiredWidth && modeInfo->height == desiredHeight && modeInfo->bpp == desiredBpp)
+  //     {
+  //       pickedMode = mode[i];
+  //       break;
+  //     }
+  //   }
 
-    if (pickedMode != 0xFFFF && VBE_SetMode(pickedMode))
-    {
-      uint32_t *fb = (uint32_t *)(modeInfo->framebuffer);
-      int w = modeInfo->width;
-      int h = modeInfo->height;
-      for (int y = 0; y < h; y++)
-      {
-        for (int x = 0; x < w; x++)
-        {
-          fb[y * modeInfo->pitch / 4 + x] = COLOR(x, y, x + y);
-        }
-      }
-    }
-  }
-  else
-  {
-    printf("No VBE extensions :(\n");
-  }
+  //   if (pickedMode != 0xFFFF && VBE_SetMode(pickedMode))
+  //   {
+  //     uint32_t *fb = (uint32_t *)(modeInfo->framebuffer);
+  //     int w = modeInfo->width;
+  //     int h = modeInfo->height;
+  //     for (int y = 0; y < h; y++)
+  //     {
+  //       for (int x = 0; x < w; x++)
+  //       {
+  //         fb[y * modeInfo->pitch / 4 + x] = COLOR(x, y, x + y);
+  //       }
+  //     }
+  //   }
+  // }
+  // else
+  // {
+  //   printf("No VBE extensions :(\n");
+  // }
 
   // execute kernel
   KernelStart kernelStart = (KernelStart)Kernel;
-  // kernelStart();
+  kernelStart();
 
 end:
   for (;;)
